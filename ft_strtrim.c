72fe026e36e1995ea58071254652664831e371ac
kazanuka <furkankuyumcu2004@gmail.com>
2024-09-21 00:19:47 +0300

    strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,11 @@
+#include "libft.h"
+char *ft_strtrim(char const *s1, char const *set)
+{
+    int i;
+    int len;
+    char *address;
+    i = 0;
+    len = ft_strlen(s1);
+    address = malloc(len);
+
+}
