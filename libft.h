33f8ac59c20f65cba94d4a2419f8041d08d631a5
kazanuka <furkankuyumcu2004@gmail.com>
2024-10-18 22:27:04 +0300

    bonus.h silindi

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fkuyumcu <fkuyumcu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 20:02:50 by fkuyumcu          #+#    #+#             */
-/*   Updated: 2024/10/14 15:43:21 by fkuyumcu         ###   ########.fr       */
+/*   Updated: 2024/10/18 22:24:20 by fkuyumcu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,5 +52,22 @@ char	**ft_split(char const *s, char c);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dest, const char *src, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t n);
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+t_list	*ft_lstnew(void *content);
+t_list	*ft_lstlast(t_list *lst);
+
+int		ft_lstsize(t_list *lst);
+
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
