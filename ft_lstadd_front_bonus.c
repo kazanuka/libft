5d094e6e48e75819b6e127d4c4143dca399021a2
kazanuka <furkankuyumcu2004@gmail.com>
2024-10-18 22:28:31 +0300

    header

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fkuyumcu <fkuyumcu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 15:13:27 by fkuyumcu          #+#    #+#             */
-/*   Updated: 2024/10/14 15:45:18 by fkuyumcu         ###   ########.fr       */
+/*   Updated: 2024/10/18 22:27:37 by fkuyumcu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "bonus.h"
+#include "libft.h"
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
